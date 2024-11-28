84fbb5356155ebd249f5d471e841180f5ef4e9f6
ciprianooooo <168427851+ciprianooooo@users.noreply.github.com>
2024-11-28 08:18:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Perícia Criminal',
+    'O que é um Perito Criminal?',
+    'Um perito criminal é um profissional especializado em investigação forense, cuja principal responsabilidade é examinar evidências físicas e fornecer conclusões científicas e técnicas em casos criminais.'
+)
+ criaCartao(
+    'Perícia Criminal',
+    'Qual é a função de um perito criminal?',
+    'É responsável pela produção da prova material, consubstanciada em laudo pericial, após a devida identificação, coleta, processamento e correta interpretação dos vestígios dentro dos limites estabelecidos pela ciência.'
+)
+
+criaCartao(
+    'Perícia Criminal',
+    'Quais as principais atividades de um Perito?',
+    'Coleta e análise de evidências físicas, Exame de armas de fogo e explosivos, Análise de documentos, Análise de drogas e substâncias químicas, Análise de imagens e vídeo, Testemunho em tribunal.'
+)
+
+criaCartao(
+    'Perícia Criminal',
+    'O que os Peritos utilizam?',
+    'Os peritos utilizam equipamentos sofisticados e técnicas avançadas de investigação forense para examinar amostras de sangue, cabelo, fibras, impressões digitais, substâncias químicas e DNA.'
+)
+
+criaCartao(
+    'Perícia Criminal',
+    'Qual o símbolo da perícia criminal?',
+    'A lupa é símbolo da perícia, já a águia, representa a visão apurada, enxerga longe e caça com estratégia, por fim, o martelo, significa a decisão judicial.'
+)
+
